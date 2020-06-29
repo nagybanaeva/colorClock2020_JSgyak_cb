816bef64f574ea4d9b3594dc40403343401fe4b7
Nagy-Bana Éva <nagybanaeva@gmail.com>
2020-06-29 15:56:38 +0200

    add alarm js

--- script.js
+++ script.js
@@ -6,30 +6,47 @@ function colorClock() {
   var minutes = date.getMinutes();
   var seconds = date.getSeconds();
 
-if (hours <= 9){
-  hours = '0' + hours;
-}
+	if (hours <= 9){
+  		hours = '0' + hours;
+	}
 
-if (minutes <= 9){
-  minutes = '0' + minutes;
-}
+	if (minutes <= 9){
+  		minutes = '0' + minutes;
+	}	
 
-if (seconds <= 9){
- seconds = '0' + seconds; 
-}
+	if (seconds <= 9){
+ 		seconds = '0' + seconds; 
+	}
 
-var hexColor = '#' + '00' + minutes + seconds;
+	var hexColor = '#' + '00' + minutes + seconds;
 
-var clockFace = (hours + ' .° ' + minutes + ' .° ' + seconds);
+	var clockFace = (hours + ' .° ' + minutes + ' .° ' + seconds);
 	
 
-document.getElementById('clock').innerHTML = clockFace;
-document.body.style.background = hexColor;
-document.getElementById('clock').style.color = 'ghostwhite';
+	document.getElementById('clock').innerHTML = clockFace;
+	document.body.style.background = hexColor;
+	document.getElementById('clock').style.color = 'white';
 
-setTimeout(function(){
-  colorClock();
-}, 1000);
+	setTimeout(function(){
+		colorClock();
+	}, 1000);
+	
+	if (minutes === 00){
+		document.getElementById('sheep').style.display = 'none';
+		document.getElementById('alarm').style.display = 'block';
+		document.getElementById('audio').play();
+	 }
+		
+	else if (minutes === 30) {
+		document.getElementById('sheep').style.display = 'block';
+		document.getElementById('alarm').style.display = 'none';
+		document.getElementById('audio').pause();
+	}
+	else {
+		document.getElementById('sheep').style.display = 'none';
+		document.getElementById('alarm').style.display = 'none';
+		document.getElementById('audio').pause();
+	}
 }
 
 colorClock();
